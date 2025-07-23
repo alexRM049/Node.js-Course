34f9b3cd20c9f702eb06aa89c5f5994e46df4628
alexrm0499 <alexrm0499@gmail.com>
2025-07-23 11:08:59 -0400

    Learning the path library

---
+++ 5.path.js
@@ -0,0 +1,20 @@
+const path = require('node:path')
+
+//path.sep returns the OS's separator
+//console.log(path.sep)
+
+// join: Join helps you to create different paths by the parameters introduced to it.
+//const filepath = path.join('content', 'subfolder', 'text.txt')
+//console.log(filepath);
+
+// basename: It returns the name of the file that's in that path.
+//const base = path.basename('\tmp/alex-secret-files\password.txt')
+//console.log(base);
+
+// This how to retrieve the name of the file without extension
+//const filename = path.basename('\tmp\alex-secret-files\password.txt', '.txt')
+//console.log(filename)
+
+//extname: it returns the name extension of a file
+//const extension = path.extname('image.jpg')
+//console.log(extension)
