c373163fe64142ebfeb5c36044c589a2e0df441b
alexrm0499 <alexrm0499@gmail.com>
2025-07-24 15:15:43 -0400

    Learning about the process library

---
+++ 7.process.js
@@ -0,0 +1,15 @@
+// data entry
+console.log(process.argv)
+
+// process control and when it ruin ends
+// process.exit(1) means the process was terminated with abnormal issue
+// process.exit(0) means the process was terminated with no issues
+//process.exit(0)
+
+// control the process events
+//process.on('exit', () => {
+//clear out resorces 
+//})
+
+// current workind directory
+console.log(process.cwd())
